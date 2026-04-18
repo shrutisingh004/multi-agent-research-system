7e59df08ad7e1d758508423ad9179fcc8d47c08a
Shruti Singh <199110053+shrutisingh004@users.noreply.github.com>
2026-04-18 13:27:22 +0530

    Update agents.py

--- agents.py
+++ agents.py
@@ -12,7 +12,7 @@ load_dotenv()
 api_key = os.getenv("GOOGLE_API_KEY") or st.secrets.get("GOOGLE_API_KEY")
 
 llm = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash",
+    model="gemini-1.5-flash",
     temperature=0,
     google_api_key=api_key
 )
